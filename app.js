59efff51f30433d449cba2b85745a9b863480846
mathias <mc2geek@gmail.com>
2014-01-06 10:03:40 -0200

    PORT

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = require('http').createServer(handler),
           io = require('socket.io').listen(app),
           fs = require('fs')
 
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
 
 function handler (req, res) {
   if (req.url == "/admin"){
