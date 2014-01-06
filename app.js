af4b46a7ff66371e1cc77e3474517e77df7fd418
mathias <mc2geek@gmail.com>
2014-01-06 09:55:22 -0200

    Procfile

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = require('http').createServer(handler),
           io = require('socket.io').listen(app),
           fs = require('fs')
 
-app.listen(80);
+app.listen(5000);
 
 function handler (req, res) {
   if (req.url == "/admin"){
